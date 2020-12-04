92df312aa56acd004fcaeb7b867dba8bbde9c758
Windsor Lima <windsorlimad@gmail.com>
2020-12-04 14:23:23 -0300

    Conectando a API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,60 +1,64 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
 import { FiChevronRight } from "react-icons/fi";
 
 import logoImg from "../../assets/logo.svg";
 import { Title, Form, Repositories } from "./styles";
+import api from "../../services/api";
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  const handleAddRepository = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo("");
+  };
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no github</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={(e) => handleAddRepository(e)}>
+        <input
+          placeholder="Digite o nome do repositório"
+          onChange={(e) => setNewRepo(e.target.value)}
+          value={newRepo}
+        />
 
         <button type="submit"> Pesquisar </button>
       </Form>
 
       <Repositories>
-        <a href="">
-          <img
-            src="https://avatars0.githubusercontent.com/u/49699668?s=460&u=cf84e7539177fe0b6d18832f37182c456f8e12f6&v=4"
-            alt="Windsor Lima"
-          />
-          <div>
-            <strong>windsorlima/sapataria_backend</strong>
-            <p>Api feita para um app sobre sapatarias</p>
-          </div>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="">
-          <img
-            src="https://avatars0.githubusercontent.com/u/49699668?s=460&u=cf84e7539177fe0b6d18832f37182c456f8e12f6&v=4"
-            alt="Windsor Lima"
-          />
-          <div>
-            <strong>windsorlima/sapataria_backend</strong>
-            <p>Api feita para um app sobre sapatarias</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="">
-          <img
-            src="https://avatars0.githubusercontent.com/u/49699668?s=460&u=cf84e7539177fe0b6d18832f37182c456f8e12f6&v=4"
-            alt="Windsor Lima"
-          />
-          <div>
-            <strong>windsorlima/sapataria_backend</strong>
-            <p>Api feita para um app sobre sapatarias</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
